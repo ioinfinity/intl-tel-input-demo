b107623f14f0883fb1a6c37da989b8d0e98bd97e
Willis Chen <ioinfinity.service@gmail.com>
2017-05-09 15:02:05 +0800

    Adds the full-phone into the Phone Input component. All library resource changes to ioinfinity/intl-tel-input/ repository.

--- defaultCountryIp.js
+++ defaultCountryIp.js
@@ -1,4 +1,6 @@
-$("#phone").intlTelInput({
+var input = $("#phone");
+
+input.intlTelInput({
   initialCountry: "auto",
   geoIpLookup: function(callback) {
     $.get('http://ipinfo.io', function() {}, "jsonp").always(function(resp) {
@@ -6,5 +8,16 @@ $("#phone").intlTelInput({
       callback(countryCode);
     });
   },
-  utilsScript: "https://rawgit.com/jackocnr/intl-tel-input/master/build/js/utils.js" // just for formatting/placeholders etc
+  utilsScript: "https://rawgit.com/ioinfinity/intl-tel-input/master/build/js/utils.js" // just for formatting/placeholders etc
+});
+
+input.on("keyup change", function() {
+
+  var intlNumber = input.intlTelInput("getNumber");
+  
+  if (intlNumber) {
+    input.attr("full-phone", intlNumber)
+  } else {
+    input.attr("full-phone", '')
+  }
 });
